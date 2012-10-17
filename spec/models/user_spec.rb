5f78773f9be43d948c8a056c7c4a37559b18676e
J G Sack <jgsack@gmail.com>
2012-10-17 13:15:47 -0700

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,14 +13,45 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
-
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   
   it { should be_valid }
+  
+  describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+  end
+  
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not == user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
+  end
+  
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
   
   describe "when name is not present" do
     before { @user.name = " " }
