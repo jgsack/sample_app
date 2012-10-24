4878a52b07e37931367a865be9a207c4ed6bbbcb
J G Sack <jgsack@gmail.com>
2012-10-24 09:23:48 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome the Sample App!"
+      redirect_to @user
+    else
+      flash[:error] = "Sorry, could not save."
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-SampleApp::Application.routes.draw do
-  get "users/new"
+SampleApp::Application.routes.draw do  
   match '/signup',  to: 'users#new'
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  resources :users
  # get "static_pages/help"
  # get "static_pages/about"
  # get "static_pages/contact"
